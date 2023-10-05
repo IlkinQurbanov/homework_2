2ad28999adcdc3f6964ee6a6d599a8d931e2374a
IlkinQurbanov <ilkin.qurbanli.92@inbox.ru>
2023-10-05 16:53:28 +0400

    Homework 2

---
+++ Homework2/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Add null ola bilmez.");
+            }
+
+            this.Name = name;
+        }
+    }
+
+    class Employee : Person
+    {
+        public double Salary { get; set; }
+        public string Position { get; set; }
+
+        public Employee(string name, double salary, string position)
+            : base(name)
+        {
+            this.Salary = salary;
+            this.Position = position;
+        }
+
+        public override string ToString()
+        {
+            return $"Ad: {Name}, Maash: {Salary}, Pozisiya: {Position}";
+        }
+    }
+
+}
---
+++ Homework2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            List<Employee> employees = new List<Employee>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string name = Console.ReadLine();
+                double salary = double.Parse(Console.ReadLine());
+                string position = Console.ReadLine();
+
+                employees.Add(new Employee(name, salary, position));
+            }
+
+            int filter = int.Parse(Console.ReadLine());
+
+            if (filter == 1)
+            {
+                string search = Console.ReadLine();
+
+                List<Employee> filteredEmployees = employees.Where(e => e.Name.ToLower().Contains(search.ToLower())).ToList();
+
+                foreach (Employee employee in filteredEmployees)
+                {
+                    Console.WriteLine(employee);
+                }
+            }
+            else
+            {
+                foreach (Employee employee in employees)
+                {
+                    Console.WriteLine(employee);
+                }
+            }
+        }
+    }
+}
